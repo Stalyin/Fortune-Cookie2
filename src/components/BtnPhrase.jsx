04e063787cd601402f073c72fe9eb73002638333
Stalyin <stalyin16ac@gmail.com>
2024-04-23 04:04:09 -0500

    newCookies3

--- src/components/BtnPhrase.jsx
+++ src/components/BtnPhrase.jsx
@@ -3,8 +3,7 @@ import getRandomNumber from "../utils/getRandomNumber";
 import quotes from "../data/phrases.json";
 import photos from "../data/photos.json";
 
-const BtnPhrase = ({ setPhraseSelected, SetBfSelected, handleClick }) => {
-  // Recibe handleClick como una prop
+const BtnPhrase = ({ setPhraseSelected, SetBfSelected }) => {
   const [buttonText, setButtonText] = useState("Toca la Galleta");
   const buttonRef = useRef(null);
 
@@ -13,7 +12,6 @@ const BtnPhrase = ({ setPhraseSelected, SetBfSelected, handleClick }) => {
     setPhraseSelected(quotes[indexRandom]);
     SetBfSelected(photos[getRandomNumber(photos.length)]);
     setButtonText("Toca otra vez");
-    handleClick();
   };
 
   useEffect(() => {
